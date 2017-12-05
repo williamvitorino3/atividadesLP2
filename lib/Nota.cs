e05d55a93fb048cf02b28b95d4adda2167e1a82e
William Vitorino <teamsifce@gmail.com>
2017-12-05 11:50:46 -0300

    Linkagem de projetos na soluction.

---
+++ lib/Nota.cs
@@ -0,0 +1,16 @@
+namespace nota
+{
+  public class Nota
+  {
+    private string id;
+    private string titulo;
+    private string conteudo;
+
+    public Nota(string id, string titulo, string conteudo)
+    {
+      this.id = id;
+      this.titulo = titulo;
+      this.conteudo = conteudo;
+    }
+  }
+}
